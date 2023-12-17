4a44551a560a37835a6e669de716e3409480b8e8
serafeim damaskos <serafeimdamaskos@gmail.com>
2023-12-17 05:24:10 +0200

    Test

--- src/main/java/com/uom/trips/service/TripService.java
+++ src/main/java/com/uom/trips/service/TripService.java
@@ -28,9 +28,9 @@ public class TripService {
 		return tripsRepository.findById(t.getTravelId());
 	}
 	
-	/*public Trip getTripById(int id) throws Exception{
+	public Trip getTripById(int id) throws Exception{
 		return tripsRepository.findById(id).get();
-	}*/
+	}
 	
 	public Optional<Trip> getTripsByArrivalLocation(Trip t) throws Exception{
 		return tripsRepository.findByArrivalLocation(t.getArrivalLocation());
